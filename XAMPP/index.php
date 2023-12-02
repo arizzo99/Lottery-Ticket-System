d13736cc6ebe6d8b0ee8ccfc53694579ba529d76
Harrison Alden King <69059239+harryking585@users.noreply.github.com>
2023-12-01 23:25:23 -0600

    Update index.php

--- XAMPP/index.php
+++ XAMPP/index.php
@@ -7,35 +7,35 @@ if(!isset($_SESSION["user"])){
 
 
 <!DOCTYPE html>
-<html lang="en">
+<html lang="en" data-bs-theme="dark">
 <head>
     <meta charset="UTF-8">
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
-    <title>User Dashboard</title>
+    <title>Home</title>
     <link rel="stylesheet" href="https://cdn.jsdelivr.net/npm/bootstrap@5.3.2/dist/css/bootstrap.min.css" integrity="sha384-T3c6CoIi6uLrA9TneNEoa7RxnatzjcDSCmG1MXxSR1GAsXEV/Dwwykc2MPK8M2HN" crossorigin="anonymous">
     <link rel="stylesheet" href="style.css">
 </head>
 <body>
 
     <h1>Texas Lottery Tickets</h1>
-    <nav class="navbar navbar-expand-lg navbar-light bg-light">
-        <a href="#.php" class="navbar-brand">TLPS</a>
+    <nav class="navbar navbar-expand-lg navbar-dark bg-dark fs-4">
+        <a href="#.php" class="navbar-brand fs-4">TLPS</a>
         <div class="collapse navbar-collapse" id="navbarNav">
             <ul class="navbar-nav">
                 <li class="nav-item active">
                     <a href="index.php" class="nav-link">Home <span class="sr-only">(current)</span></a>
                 </li>
                 <li class="nav-item">
-                    <a href="#.php" class="nav-link">Browse </a>
+                    <a href="browse.php" class="nav-link">Browse </a>
                 </li>
                 <li class="nav-item">
-                    <a href="#.php" class="nav-link">Ticket Search </a>
+                    <a href="ticket_search.php" class="nav-link">Ticket Search </a>
                 </li>
                 <li class="nav-item">
-                    <a href="#.php" class="nav-link">My Profile </a>
+                    <a href="profile.php" class="nav-link">My Profile </a>
                 </li>
                 <li class="nav-item">
-                    <a href="#.php" class="nav-link">History </a>
+                    <a href="history.php" class="nav-link">History </a>
                 </li>
                 <li class="nav-item">
                     <a href="logout.php" class="nav-link">Logout</a>
@@ -45,8 +45,9 @@ if(!isset($_SESSION["user"])){
                 </li>
             </ul>
         </div>
-    
     </nav>
+
+    <h4>Next ticket drawing in : </h4>
     
 
 </body>
